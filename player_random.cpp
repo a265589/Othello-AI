db68aa0454b6e5365fe2e9ebed7165c0454160cb
a265589 <41287347+a265589@users.noreply.github.com>
2020-06-26 12:24:31 +0800

    Update player_random.cpp

--- player_random.cpp
+++ player_random.cpp
@@ -5,14 +5,247 @@
 #include <cstdlib>
 #include <ctime>
 
-struct Point {
-    int x, y;
+
+
+
+
+
+#define search_depth 3
+#define H_BASE 1000000
+
+
+
+
+
+enum SPOT_STATE {
+    EMPTY = 0,
+    BLACK = 1,
+    WHITE = 2
 };
+
+int get_next_player(int player) {
+    return 3 - player;
+}
+
+
+
+class Point {
+public:
+    int x, y;
+  //  Point() : Point(0, 0) {}
+    Point(int x, int y) : x(x), y(y) {}
+    bool operator==(const Point& rhs) const {
+        return x == rhs.x && y == rhs.y;
+    }
+    bool operator!=(const Point& rhs) const {
+        return !operator==(rhs);
+    }
+    Point operator+(const Point& rhs) const {
+        return Point(x + rhs.x, y + rhs.y);
+    }
+    Point operator-(const Point& rhs) const {
+        return Point(x - rhs.x, y - rhs.y);
+    }
+};
+
 
 int player;
 const int SIZE = 8;
 std::array<std::array<int, SIZE>, SIZE> board;
 std::vector<Point> next_valid_spots;
+
+const std::array<Point, 8> directions{ {
+       Point(-1, -1), Point(-1, 0), Point(-1, 1),
+       Point(0, -1), /*{0, 0}, */Point(0, 1),
+       Point(1, -1), Point(1, 0), Point(1, 1)
+   } };
+
+
+
+class option
+{
+public:
+     int board[SIZE][SIZE];
+     std::array<int, 3> disc_count;
+
+    option()
+    {
+
+         disc_count[0] = 0;
+         disc_count[1] = 0;
+         disc_count[2] = 0;
+
+
+    }
+
+    ~option()
+    {
+
+    }
+
+
+    bool is_spot_valid(Point center, int cur_player) {
+        if (get_disc(center) != EMPTY)
+            return false;
+        for (Point dir : directions) {
+            // Move along the direction while testing.
+            Point p = center + dir;
+            if (!is_disc_at(p, get_next_player(cur_player)))
+                continue;
+            p = p + dir;
+            while (is_spot_on_board(p) && get_disc(p) != EMPTY) {
+                if (is_disc_at(p, cur_player))
+                    return true;
+                p = p + dir;
+            }
+        }
+        return false;
+    }
+
+    int get_disc(Point p) {
+        return board[p.x][p.y];
+    }
+
+    bool is_disc_at(Point p, int disc) {
+        if (!is_spot_on_board(p))
+            return false;
+        if (get_disc(p) != disc)
+            return false;
+        return true;
+    }
+
+    bool is_spot_on_board(Point p) {
+        return 0 <= p.x && p.x < SIZE && 0 <= p.y && p.y < SIZE;
+    }
+
+      void flip_discs(Point center,int cur_player) {
+
+         for (Point dir : directions) {
+
+             // Move along the direction while testing.
+
+             Point p = center + dir;
+
+             if (!is_disc_at(p, get_next_player(cur_player)))
+
+                 continue;
+
+             std::vector<Point> discs({ p });
+
+             p = p + dir;
+
+             while (is_spot_on_board(p) && get_disc(p) != EMPTY) {
+
+                 if (is_disc_at(p, cur_player)) {
+
+                     for (Point s : discs) {
+
+                         set_disc(s, cur_player);
+
+                     }
+
+                   //  disc_count[cur_player] += discs.size();
+
+                     //disc_count[get_next_player(cur_player)] -= discs.size();
+
+                     break;
+
+                 }
+
+                 discs.push_back(p);
+
+                 p = p + dir;
+
+             }
+
+         }
+
+     }
+
+    void set_disc(Point p, int disc) {
+
+        board[p.x][p.y] = disc;
+
+    }
+
+
+    int evaluate()
+    {
+        return  disc_count[player] * 10 - disc_count[get_next_player(player)] * 10;
+    }
+
+};
+
+
+
+int minimax(option now, int depth, bool MAXIMIZE, int cur_player)
+{
+
+    std::vector<Point> next_possible_step;
+
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            Point p = Point(i, j);
+            if (now.board[i][j] != EMPTY)
+                continue;
+            if (now.is_spot_valid(p, cur_player))
+                next_possible_step.push_back(p);
+        }
+    }
+
+
+    for (auto p : next_possible_step)
+    {
+        option next;
+
+        //next.flip_discs(p, cur_player);
+
+        if (depth == 0)
+        {
+            return minimax(next, depth + 1, !MAXIMIZE, get_next_player(cur_player));
+        }
+        else if (depth == search_depth)
+        {
+            return now.evaluate();
+        }
+        else if (MAXIMIZE == true)
+        {
+
+            int H_max = -H_BASE;
+
+            int H = minimax(next, depth + 1, !MAXIMIZE, get_next_player(cur_player));
+
+            if (H > H_max)
+            {
+                H_max = H;
+            }
+
+            return H_max;
+        }
+        else if (MAXIMIZE == false)
+        {
+
+            int H_min = H_BASE;
+
+
+            int H = minimax(next, depth + 1, !MAXIMIZE, get_next_player(cur_player));
+
+            if (H < H_min)
+            {
+                H_min = H;
+            }
+
+            return H_min;
+
+        }
+
+
+    }
+
+
+}
+
+
 
 void read_board(std::ifstream& fin) {
     fin >> player;
@@ -29,20 +262,75 @@ void read_valid_spots(std::ifstream& fin) {
     int x, y;
     for (int i = 0; i < n_valid_spots; i++) {
         fin >> x >> y;
-        next_valid_spots.push_back({x, y});
+        next_valid_spots.push_back({ x, y });
     }
 }
 
+
+
 void write_valid_spot(std::ofstream& fout) {
     int n_valid_spots = next_valid_spots.size();
-    srand(time(NULL));
+    //srand(time(NULL));
     // Choose random spot. (Not random uniform here)
-    int index = (rand() % n_valid_spots);
-    Point p = next_valid_spots[index];
+    //int index = (rand() % n_valid_spots);
+    int max_H = -100000;
+    int next_step_id = 0;
+
+   
+
+    for (int i = 0; i < n_valid_spots; i++)
+    {
+        Point p = next_valid_spots[i];
+
+        option start;
+
+     
+
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+              start.board[i][j] = board[i][j];
+
+              start.disc_count[board[i][j]]++;
+            }
+        }
+
+
+
+         start.flip_discs(p, player);
+
+        int H = minimax(start, 0, true, get_next_player(player));
+
+        if ( H > max_H)
+        {
+             max_H = H;
+             next_step_id = i;
+        }
+
+    }
+
+    Point p = next_valid_spots[next_step_id];
     // Remember to flush the output to ensure the last action is written to file.
     fout << p.x << " " << p.y << std::endl;
     fout.flush();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 int main(int, char** argv) {
     std::ifstream fin(argv[1]);
@@ -54,3 +342,12 @@ int main(int, char** argv) {
     fout.close();
     return 0;
 }
+
+
+
+
+
+
+
+
+
